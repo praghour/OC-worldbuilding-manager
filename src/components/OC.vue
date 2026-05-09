0aa16e7a62bfd95a2fffceab4a1f8b812ba1fb09
praghour <ketaikee@yandex.ru>
2026-05-10 00:03:12 +0300

    add OC structure

---
+++ src/components/OC.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+    <div>Страница персонажей</div>
+    <RouterLink :to="{ name: 'oc_profile' }">Профиль персонажа</RouterLink>
+    <div class="app">
+        <RouterView />
+    </div>
+</template>
+
+<style scoped>
+</style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,8 +1,12 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-
+    <div>Главная страница</div>
+    <div class="pages">
+        <RouterLink :to="{ name: 'oc' }">Персонажи</RouterLink>
+    </div>
 </template>
 
 <style scoped>
